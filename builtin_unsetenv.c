a809af71af65b2ef96cd0f17300dd42440fda3f2
Fedevaz <favazquez@correo.um.edu.uy>
2022-06-16 11:22:47 -0300

    add builtin_unsetenv.c

--- builtin_unsetenv.c
+++ builtin_unsetenv.c
@@ -8,9 +8,15 @@
 int builtin_unsetenv(int argc, char **argv){
     if (argc ==1){
         fprintf(stderr, "%s\n", "ERROR: Not enough input arguments");
+        return EXIT_FAILURE;
     }
 
     for (int i = 1; i<argc; i++){
-        unsetenv(argv[i]);
+        if(getenv(argv[i]) == NULL || (unsetenv(argv[i])!=0)){
+            return EXIT_FAILURE;
+        }
     }
+    return EXIT_SUCCESS;
+    
+
 }
