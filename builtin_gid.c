9ea8134b93265ed3d2035852f5727df60f77ec22
JMHermidaV <manuhermida@gmail.com>
2022-06-20 17:34:49 -0300

    corrijo prints de gid

--- builtin_gid.c
+++ builtin_gid.c
@@ -3,11 +3,11 @@ int builtin_gid (int argc, char ** argv){
 if(argc ==1){
 
 
-    printf("La ID del grupo principal es: %d \n Nombre del grupo: %s ", getgid(), getgrgid(getgid())->gr_name);
+    printf("La ID del grupo principal es: %d  Nombre del grupo: %s \n", getgid(), getgrgid(getgid())->gr_name);
     gid_t grouplist[NGROUPS_MAX];
     int ngroups = getgroups(NGROUPS_MAX, grouplist);
     for(int i=0; i < ngroups; i++){
-        printf("La ID del grupo secundaro %d es: %d - Nombre del grupo: %s", i, grouplist[i],getgrgid(grouplist[i])->gr_name);
+        printf("La ID del grupo secundario %d es: %d - Nombre del grupo: %s\n", i, grouplist[i],getgrgid(grouplist[i])->gr_name);
     }
     return 0; 
 }
